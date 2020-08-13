5f08a93e76c6ef212f4b32388f2d1eead26d308e
Nebojša Jakovljević <nebojsa@nebjak.net>
2020-08-13 21:59:09 +0200

    initial commit, comparison operators

---
+++ src/comparison_operators.js
@@ -0,0 +1,49 @@
+const { MongoClient } = require('mongodb');
+
+const DB_URL = 'mongodb://localhost:27017';
+const DB_NAME = 'emakinaks';
+const DB_COLLECTION = 'movies';
+
+const config = {
+  useUnifiedTopology: true,
+};
+
+(async () => {
+  try {
+    const client = await MongoClient.connect(DB_URL, config);
+    console.log('🔛 Connected to DB\n------------------\n\n');
+
+    const collection = client.db(DB_NAME).collection(DB_COLLECTION);
+
+    // runtime attribute less or equall to 30
+    const runtime30orLess = await collection
+      .find({
+        runtime: { $lte: 30 },
+      })
+      .count();
+    console.log(`🎥 ${runtime30orLess} movies, 30 minutes or shorter.\n\n`);
+
+    // runtime attribute greater than 90
+    const runtimeGreaterThan90 = await collection
+      .find({
+        runtime: { $gt: 30 },
+      })
+      .count();
+    console.log(`🎬 ${runtimeGreaterThan90} movies, longer than 90 minutes.\n\n`);
+
+    // movies released on round years
+    const years = [1950, 1960, 1970, 1980, 1990, 2000];
+    const roundYears = await collection
+      .find({
+        year: { $in: years },
+      })
+      .count();
+    console.log(`📽 ${roundYears} movies, released in round years. [${years.join(', ')}]\n\n`);
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  } finally {
+    console.log('👋 bye!');
+    process.exit(0);
+  }
+})();
